5af04f7f838c3218c8b0e9d857178c90ed8d08d9
Aleksandr <ZeroDay9898@gmail.com>
2021-03-27 20:25:27 +0300

    Adaptive landing page - SipHome

--- SipHome/js/script.js
+++ SipHome/js/script.js
@@ -1,18 +1,33 @@
+const body = document.querySelector("body");
+
+let widtwWindowScroll = innerWidth - body.offsetWidth;
+let blocksStopScrolling = document.querySelectorAll(".lock-padding");
+let timeoutTransition = 200;
+
+
 // block - header
 const header = document.querySelector(".header");
+const navLinkItems = header.querySelectorAll(".header__content-nav-item-link")
+const navLinkItemLast = navLinkItems[navLinkItems.length - 1];
+
+const headerBtnMenu = document.querySelector(".header__content-back-btn-menu");
+
+
+// block - mobile-nav
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavContent = document.querySelector(".mobile-nav__content");
 
 
 // block - intro
 const intro = document.querySelector(".intro");
+const btnMenuClose = document.querySelector(".mobile-nav__content-back-btn-close");
 
 
 // block - testimonials
 const testimonialsContentItems = document.querySelector(".testimonials__content-items");
 const testimonialsContentItem = document.querySelector(".testimonials__content-item");
-let widthTestimonialsContentItem = testimonialsContentItem.offsetWidth;
-
-let marginRightTestimonialsContentItem = getComputedStyle(testimonialsContentItem).marginRight;
-marginRightTestimonialsContentItem = Number(marginRightTestimonialsContentItem.replace("px", ""));
+const testimonialsContentItemsCount = document.querySelectorAll(".testimonials__content-item");
+const testimonialsContentItemLast = testimonialsContentItemsCount[testimonialsContentItemsCount.length - 1];
 
 const testimonialsBtnPrev = document.querySelector(".testimonials-btn-prev");
 const testimonialsBtnNext = document.querySelector(".testimonials-btn-next");
@@ -24,17 +39,70 @@ const testimonialsBtnPoint_3 = document.querySelector("#testimonials-content-btn
 const testimonialsBtnPoint_4 = document.querySelector("#testimonials-content-btn-4");
 
 
-// block - header and intro
+// block - contact
+const contactSelectBox = document.querySelector(".contact__content-form-back-select-box");
+const contactSelectBoxContainer = document.querySelector(".contact__content-form-options-container"); 
 
+const contactSelectedBox = document.querySelector(".contact__content-form-selected");
+const contactSelectBoxOptions = document.querySelectorAll(".contact__content-form-option");
+
+
+// block - header and intro
 window.addEventListener("scroll", () => {
 
     if (pageYOffset >= 100) {
         header.classList.add("header-active");
+        header.classList.add("lock-padding");
+
+        mobileNavContent.classList.add("mobile-nav-content-active");
+
         intro.classList.add("intro-active");
+        navLinkItemLast.classList.add("header__content-nav-item-link-pass");
+
+        blocksStopScrolling = document.querySelectorAll(".lock-padding");
+    
     } else {
         header.classList.remove("header-active");
+        header.classList.remove("lock-padding");
+
+        mobileNavContent.classList.remove("mobile-nav-content-active");
+
         intro.classList.remove("intro-active");
+        navLinkItemLast.classList.remove("header__content-nav-item-link-pass");
+
+        blocksStopScrolling = document.querySelectorAll(".lock-padding");
     };
+});
+
+headerBtnMenu.addEventListener("click", () => {
+    mobileNav.classList.add("mobile-nav-active");
+
+    for (let index = 0; index < blocksStopScrolling.length; index++) {
+        const elementLock = blocksStopScrolling[index];
+        elementLock.style.paddingRight = widtwWindowScroll + "px";
+        console.log(1);
+    };
+
+    body.style.paddingRight = widtwWindowScroll + "px";
+    body.classList.add("body-pass");
+});
+
+
+// block - mobile-nav
+btnMenuClose.addEventListener("click", () => {
+    mobileNav.classList.remove("mobile-nav-active");
+
+    setTimeout(() => {
+
+        for (let index = 0; index < blocksStopScrolling.length; index++) {
+            const elementLock = blocksStopScrolling[index];
+            elementLock.style.paddingRight = "0px";
+        };
+
+        body.style.paddingRight = "0px";
+        body.classList.remove("body-pass");
+
+    }, timeoutTransition * 2);
 });
 
 
@@ -46,21 +114,43 @@ let positionPressedFirstScore = 1;
 let positionScore = 0;
 let positionScorePoint = 0;
 
+let widthTestimonialsContentItem = testimonialsContentItem.offsetWidth;
+let marginRightTestimonialsContentItem = getComputedStyle(testimonialsContentItem).marginRight;
+marginRightTestimonialsContentItem = Number(marginRightTestimonialsContentItem.replace("px", ""));
+
+checkResiveWidthWindow()
+window.addEventListener("resize", () => { checkResiveWidthWindow() });
+
+function checkResiveWidthWindow() {
+    if (innerWidth <= 840) {
+        testimonialsContentItemLast.classList.add("testimonials-content-item-pass");
+    } else {
+        testimonialsContentItemLast.classList.remove("testimonials-content-item-pass");
+    };
+};
+
 testimonialsBtnPrev.addEventListener("click", () => {
-    pressedBtn(position = widthTestimonialsContentItem, marginRight = marginRightTestimonialsContentItem, score = -1);
+    pressedBtn(position = widthTestimonialsContentItem,
+               marginRight = marginRightTestimonialsContentItem,
+               score = -1,
+               scorePoint = -1);
 });
 
 testimonialsBtnNext.addEventListener("click", () => {
-    pressedBtn(position = -widthTestimonialsContentItem, marginRight = -marginRightTestimonialsContentItem, score = 1);
+    pressedBtn(position = -widthTestimonialsContentItem,
+               marginRight = -marginRightTestimonialsContentItem,
+               score = 1,
+               scorePoint = 1);
 });
 
-function pressedBtn(position, marginRight, score) {
+function pressedBtn(position, marginRight, score, scorePoint) {
     positionLeft += position;
     positionLeft += marginRight;
 
     testimonialsContentItems.style.left = positionLeft + "px";
 
     positionScore += score;
+    positionScorePoint += scorePoint;
 
     testimonialsBtnsPoints.forEach( (btn) => {
         if (btn.classList.contains("testimonials-btn-active")) {
@@ -100,7 +190,7 @@ function pressedBtnPoint(positionLeft, positionScore, btnPoint) {
     testimonialsContentItems.style.left = positionLeft + "px";
 
     positionScorePoint = positionScore;
-    
+
     testimonialsBtnsPoints.forEach( (btn) => {
         if (btn.classList.contains("testimonials-btn-active")) {
             btn.classList.remove("testimonials-btn-active");
@@ -114,7 +204,7 @@ function pressedBtnPoint(positionLeft, positionScore, btnPoint) {
 
 
 function checkPressedBtn() {
-    if (positionScore == 0) {
+    if (positionScore == 0 || positionScorePoint == 0) {
         testimonialsBtnPrev.classList.remove("testimonials-content-items-btn-active");
         testimonialsBtnNext.classList.add("testimonials-content-items-btn-active")
     }
@@ -128,3 +218,16 @@ function checkPressedBtn() {
         testimonialsBtnNext.classList.add("testimonials-content-items-btn-active")
     }
 };
+
+
+// block - contact
+contactSelectBox.addEventListener("click", () => {
+    contactSelectBoxContainer.classList.toggle("contact-content-form-options-container-active");
+
+    contactSelectBoxOptions.forEach( (item) => {
+        item.addEventListener("click", () => {
+            contactSelectedBox.innerHTML = item.querySelector("label").innerHTML;
+            contactSelectBox.classList.remove("contact-content-form-options-container-active");
+        });
+    });
+});
